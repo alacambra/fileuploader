6bc8626452e39e020be8cb03fd42bf8ca70cd9a4
alacambra <alacambra@gmail.com>
2015-01-11 02:00:20 +0100

    first import

---
+++ server/src/main/java/alacambra/uploadmanager/server/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package alacambra.uploadmanager.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@ApplicationPath("r")
+public class JAXRSConfiguration extends Application {
+
+}
---
+++ server/src/main/java/alacambra/uploadmanager/server/boundary/FileUploader.java
@@ -0,0 +1,92 @@
+package alacambra.uploadmanager.server.boundary;
+
+import org.apache.commons.io.IOUtils;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import javax.ejb.Stateful;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by alacambra on 1/11/15.
+ */
+@Path("file")
+@Stateless
+public class FileUploader {
+
+    private final String UPLOADED_FILE_PATH = "/home/alacambra/removeme/";
+
+    @POST
+    @Consumes("multipart/form-data")
+    public void file(MultipartFormDataInput input){
+        String fileName = "";
+
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+        List<InputPart> inputParts = uploadForm.get("file");
+
+        for (InputPart inputPart : inputParts) {
+
+            try {
+
+                MultivaluedMap<String, String> header = inputPart.getHeaders();
+                fileName = getFileName(header);
+
+                //convert the uploaded file to inputstream
+                InputStream inputStream = inputPart.getBody(InputStream.class,null);
+                byte [] bytes = IOUtils.toByteArray(inputStream);
+
+                //constructs upload file path
+                fileName = UPLOADED_FILE_PATH + fileName;
+                writeFile(bytes,fileName);
+                System.out.println("Done");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+//        return Response.ok().build();
+    }
+
+    private String getFileName(MultivaluedMap<String, String> header) {
+
+        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("name"))) {
+
+                String[] name = filename.split("=");
+                String finalFileName = name[1].trim().replaceAll("\"", "");
+
+                return finalFileName;
+            }
+        }
+        return header.getFirst("Content-Disposition");
+    }
+
+    private void writeFile(byte[] content, String filename) throws IOException {
+
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileOutputStream fop = new FileOutputStream(file);
+
+        fop.write(content);
+        fop.flush();
+        fop.close();
+    }
+}
